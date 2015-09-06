5b5c2214efec0680255baee898329c4569902d3f
Ross Tuck <me@rosstuck.com>
2015-09-06 20:51:41 +0200

    Add tuck/doctrine-event-store supportAdd tuck/doctrine-event-store supportAdd tuck/doctrine-event-store supportAdd tuck/doctrine-event-store supportAdd tuck/doctrine-event-store supportAdd tuck/doctrine-event-store supportAdd tuck/doctrine-event-store supportAdd tuck/doctrine-event-store supportAdd tuck/doctrine-event-store support

--- di.php
+++ di.php
@@ -3,9 +3,8 @@ require_once 'vendor/autoload.php';
 /** @var \Doctrine\ORM\EntityManager $entityManager */
 $entityManager = require_once __DIR__ . '/bootstrap.php';
 use Pimple\Container;
+
 $container = new Container();
-
-
 
 $container['entity_manager'] = function (Container $container) use ($entityManager) {
     return $entityManager;
@@ -14,5 +13,24 @@ $container['entity_manager'] = function (Container $container) use ($entityManag
 $container['db_connection'] = function () use ($entityManager) {
     return $entityManager->getConnection();
 };
+
+$container['event_dispatcher'] = function () {
+    return new Tuck\EventDispatcher\BufferedEventDispatcher(
+        new Tuck\EventDispatcher\SimpleEventDispatcher()
+    );
+};
+$container['event_store'] = function ($c) {
+    return new Broadway\EventStore\DBALEventStore(
+        $c['db_connection'],
+        new \Broadway\Serializer\SimpleInterfaceSerializer(),
+        new \Broadway\Serializer\SimpleInterfaceSerializer(),
+        'Events'
+    );
+};
+$container['event_collector'] = function ($c) {
+    $eventUoW = new \Tuck\DoctrineEventStore\UnitOfWork($c['event_store'], $c['event_dispatcher']);
+    return new Tuck\DoctrineEventStore\EventCollector($eventUoW);
+};
+$entityManager->getEventManager()->addEventSubscriber($container['event_collector']);
 
 return $container;
